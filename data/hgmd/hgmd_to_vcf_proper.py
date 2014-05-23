5bfefbef33146556507a7b93917427dae0287a57
Tal Friedman <talf301@gmail.com>
2014-05-23 12:22:59 -0400

    added diseases to hgmd parsing, chromosome-wise splitting to vcf parsing

--- data/hgmd/hgmd_to_vcf_proper.py
+++ data/hgmd/hgmd_to_vcf_proper.py
@@ -26,6 +26,8 @@ def load_genome(filename):
             genome[name] = ''.join(sequences)
     return genome
 
+
+
 #load hgmd and dummy values we'll need
 def load_hgmd(filename):
     hgmd = []
@@ -33,15 +35,16 @@ def load_hgmd(filename):
         for line in file:
             if line[0] == '#':continue
             info = line.split('\t')
-            hgmd.append([info[0]] + [info[1]] + ['.'] + info[2:4] + ['50','PASS','.','GT','./.'])
+            hgmd.append([info[0]] + [info[1]] + ['.'] + info[2:4] + ['50','PASS','.','GT','./.']+info[5:7])
     return hgmd 
 def load_vcf(filename):
-    vcf = []
+    vcf = {}
     with gzip.open(filename) as file:
         for line in file:
             if line[0] == '#':continue
             info = line.split('\t')
-            vcf.append(['chr'+info[0]] + [info[1]] + [info[3]])
+            chr = 'chr' + info[0]
+            vcf[chr].append([chr] + [info[1]] + info[3:4])
     return vcf
 
 def create_fa(filename,hgmd):
@@ -52,6 +55,9 @@ def create_fa(filename,hgmd):
         out.write(l[3] + '\n')
     out.close()
 
+def get_correct(hgmd, genome):
+    return filter(lambda x: x[3] and genome[x[0]][int(x[1])-1:int(x[1])+len(x[3])-1].upper() == x[3].upper(), hgmd)
+
 def test_accuracy(hgmd, genome):
     counter = 0
     for h in hgmd:
@@ -61,27 +67,9 @@ def test_accuracy(hgmd, genome):
     
 if __name__ == '__main__':
     #genome = load_genome('/filer/hg18/hg18.fa')
-    #hgmd = load_hgmd('hgmd_pro_allmut_2013.4')
-    #genome = load_genome('/filer/hg19/hg19.fa')    
-#counter = 0
-   # for h in hgmd:
-    #    if h[3] and h[3] != '-' and genome[h[0]][int(h[1])-1:int(h[1])+len(h[3])-1].upper() != h[3]:
-           #print h[3]
-           #print genome[h[0]][int(h[1])-1:int(h[1])+len(h[3])-1] 
-     #      counter = counter + 1
-       # if i > 20: break
-    #print 'hg18:'
-    #print counter
-    #print float(counter)/float(len(hgmd))
-    #counter = 0 
-    #genome2 = load_genome('/filer/hg19/hg19.fa') 
-    #for h in hgmd:
-    #    if genome2[h[0]][int(h[1])-1:int(h[1])+len(h[3])-1] != h[3]:
-    #        counter = counter + 1
-       
-    #print 'hg19:'
-    #print counter
-    #print float(counter)/float(len(hgmd))
+    hgmd = load_hgmd('hgmd_pro_allmut_2013.4')
+    #genome = load_genome('/filer/hg19/hg19.fa') 
+    #dbsnp = load_vcf('dbSnp.vcf')   
     #vcf = load_vcf('/dupa-filer/talf/matchingsim/data/1000gp/samples/complete/HG00096.vcf.gz')
     #for v in vcf:
     #    if genome[v[0]][int(v[1])-1:int(v[1])+len(v[2])-1].upper() != v[2]:
@@ -89,5 +77,4 @@ if __name__ == '__main__':
 
    # print counter
    # print float(counter)/float(len(vcf))
-
    
