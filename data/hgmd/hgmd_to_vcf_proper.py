6f7155edcb05644f976b79e0b3e139f1d1605e6a
Tal Friedman <talf301@gmail.com>
2014-05-26 15:22:58 -0400

    added vcf writing, etc

--- data/hgmd/hgmd_to_vcf_proper.py
+++ data/hgmd/hgmd_to_vcf_proper.py
@@ -27,15 +27,16 @@ def load_genome(filename):
     return genome
 
 class Entry:
-    def __init__(self, chrom, loc, ref, alt, pmid):
+    def __init__(self, chrom, loc, ref, alt, pmid,omimid):
         self.chrom = chrom
         self.loc = loc
         self.ref = ref
         self.alt = alt
         self.pmid = pmid
+        self.omimid = omimid
     
     def __str__(self):
-        return [self.chrom, self.loc, self.ref, self.alt, self.pmid].__str__()
+        return [self.chrom, self.loc, self.ref, self.alt, self.pmid, self.omimid].__str__()
 
     def __repr__(self):
         return self.__str__()
@@ -46,18 +47,26 @@ class HGMD:
     
     def __iter__(self):
         return iter(self.entries)
-   
+    
+    @classmethod
+    def write_vcf(cls, filename, entries):
+        with open(filename, 'w') as out:
+            out.write('##fileformat=VCFv4.1\n')
+            out.write('\t'.join(['#CHROM','POS','ID','REF','ALT','QUAL','FILTER', 'INFO', 'FORMAT', 'FIRST']) + '\n') 
+            for e in entries:
+                out.write('\t'.join([e.chrom,e.loc,'.',e.ref,e.alt,'50','PASS','OMIM:' + e.omimid + ';PM:' + e.pmid,'GT','./.']))
+     
     def iter_lines(self, filename):
          with open(filename) as hgmd:
             for line in hgmd:
                 if line[0] == '#':continue
                 info = line.split('\t')
-                yield Entry(info[0],info[1],info[2],info[3],info[6])
+                if len(info) < 8:continue
+                yield Entry(info[0],info[1],info[2],info[3],info[6], info[7])
 
     def __str__(self):
         return self.entries.__str__()
 
-#load hgmd and dummy values we'll need
 def load_hgmd(filename):
     return HGMD(filename).entries 
 
@@ -102,6 +111,9 @@ def create_fa(filename,hgmd):
 def get_correct(hgmd, genome):
     return filter(lambda x: x.ref and genome[x.chrom][int(x.loc)-1:int(x.loc)+len(x.ref)-1].upper() == x.ref.upper(), hgmd)
 
+def get_long(hgmd):
+    return filter(lambda x: len(x.ref) >= 5, hgmd)  
+
 def test_accuracy(hgmd, genome):
     counter = 0
     for h in hgmd:
@@ -123,10 +135,11 @@ def get_unique_diseases(hgmd):
     for h in hgmd:
         s.add(h.pmid)
     return len(s)  
- 
+
 if __name__ == '__main__':
     #genome = load_genome('/filer/hg18/hg18.fa')
     hgmd = load_hgmd('hgmd_pro_allmut_2013.4')
+    
     #genome = load_genome('/filer/hg19/hg19.fa') 
     #dbsnp = load_vcf('dbSnp.vcf')   
     #vcf = load_vcf('/dupa-filer/talf/matchingsim/data/1000gp/samples/complete/HG00096.vcf.gz')
