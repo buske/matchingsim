4d6855d291a1fb0551e6badc561a610428617f5d
Tal Friedman <talf301@gmail.com>
2014-06-10 17:20:00 -0400

    fixed memory issue, added copy handling for uncompressed vcf

--- patients/randompatients/gen_exomiser1_random.sh
+++ patients/randompatients/gen_exomiser1_random.sh
@@ -5,7 +5,7 @@ set -o pipefail
 #Use date and time as a signature for the generated data
 sig=`date +%F-%H-%M-%S`
 #sge stuff
-memory=4G
+memory=10G
 processors=1
 logdir=~/sge_logs/gen_exomise/$sig/
 mkdir -pv $logdir
@@ -16,15 +16,15 @@ loc=$1
 num=$2
 #step 1: make copies of some files (randomly choose the number specified)
 mkdir -pv $out
-for s in `ls $loc/*.vcf.gz | sort -R | head -n $num`
+for s in `ls $loc/*.vcf.gz $loc/*.vcf | sort -R | head -n $num`
 do
     cp $s $out 
 done
-#step 2: run our patient generator to make "infect" all of these patients
+#step 2: run our patient generator to "infect" all of these patients
 python $data/randompatients/generate_patients.py $data/phenotype_annotation.tab $data/hgmd_correct.jv.vcf $out $data/orphanet_lookup.xml $data/orphanet_inher.xml $data/orphanet_geno_pheno.xml -I AD
 #step 3: create a script and dispatch exomizer job (and create rerun script)
 cat > "$out/rerun.sh" <<EOF
-for s in \`$out/scripts/*.sh\`
+for s in $out/scripts/*.sh
 do
     qsub -S /bin/sh \$s
 done
@@ -52,7 +52,8 @@ temp=\$TMPDIR/$f.ezr
 #Run exomizer, only if the required file doesn't already exist
 if [ ! -f "$out"/$f.ezr ]
 then
-    gunzip $out/$f.vcf.gz | java -Xmx1900m -Xms1000m -jar /data/Exomiser/Exomizer.jar --db_url jdbc:postgresql://combine-102.biolab.sandbox/nsfpalizer -D /data/Exomiser/ucsc.ser -I AD -F 1 --hpo_ids `cat $out/"$f"_hpo.txt` -v $out/$f.vcf --vcf_output -o \$temp
+    gunzip $out/$f.vcf.gz
+    java -Xmx1900m -Xms1000m -jar /data/Exomiser/Exomizer.jar --db_url jdbc:postgresql://combine-102.biolab.sandbox/nsfpalizer -D /data/Exomiser/ucsc.ser -I AD -F 1 --hpo_ids `cat $out/"$f"_hpo.txt` -v $out/$f.vcf --vcf_output -o \$temp
 fi
 
 mv -v \$temp $out/$f.ezr.temp
