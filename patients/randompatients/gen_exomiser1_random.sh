c586212831f7df624e5d698d470ae7fc3df0b03f
Tal Friedman <talf301@gmail.com>
2014-06-10 16:07:10 -0400

    Added extra safety steps and exomiser1 running

--- patients/randompatients/gen_exomiser1_random.sh
+++ patients/randompatients/gen_exomiser1_random.sh
@@ -23,11 +23,12 @@ done
 #step 2: run our patient generator to make "infect" all of these patients
 python /dupa-filer/talf/matchingsim/patients/randompatients/generate_patients.py /dupa-filer/talf/matchingsim/patients/phenotype_annotation.tab /dupa-filer/talf/matchingsim/data/hgmd/hgmd_correct.jv.vcf $out
 #step 3: create a script and dispatch exomizer job
+mkdir -pv $out/scripts
 for file in $out/*.vcf.gz; do
     #create a bash script
     #get only ending to name script
     f=`echo $file | rev | cut -d '/' -f1 | rev | cut -d '.' -f1`
-    script="$out/dispatch_$f.sh"   
+    script="$out/scripts/dispatch_$f.sh"   
     cat > "$script" <<EOF
 #!/usr/bin/env bash
 #$ -V
@@ -39,8 +40,9 @@ for file in $out/*.vcf.gz; do
 
 set -eu
 set -o pipefail
+temp=\$TMPDIR/
 
-#Run exomizer
+#Run exomizer, only if the required file doesn't already exist
 gunzip $out/$f.vcf.gz | java -Xms5g -Xmx5g -jar /filer/tools/exomiser/2.0.0/exomiser-2.0.0.jar -D /filer/tools/exomiser/2.0.0/data/ucsc_hg19.ser -I AD -F 1 -W /filer/tools/exomiser/2.0.0/data/rw_string_9_05.gz -X /filer/tools/exomiser/2.0.0/data/rw_string_9_05_id2index.gz --hpo_ids `cat $out/"$f"_hpo.txt` -v $out/$f.vcf --vcf_output 
 
 touch $out/$f.complete
