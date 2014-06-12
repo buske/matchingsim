82ce4ab89e25caaf74ce264241dfc626d3f8ee89
Tal Friedman <talf301@gmail.com>
2014-06-12 11:09:13 -0400

    updated score logging for both std out and file

--- patients/analysis/fetch_scores.py
+++ patients/analysis/fetch_scores.py
@@ -6,6 +6,7 @@ __author__ = 'Tal Friedman'
 
 import os
 import sys
+import logging
 
 def get_last_line(path):
     with open(path) as file:
@@ -20,13 +21,20 @@ def get_first_line(path):
             return line
 
 def script(vcf_ezr_path):
+    logging.basicConfig(filename = os.path.join(vcf_ezr_path, 'score.log'), level = logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logging.getLogger().addHandler(ch)
+
     counter = 0
     contents = os.listdir(vcf_ezr_path)
     vcf_files = filter(lambda f: f.endswith('.vcf'),contents)
     ezr_files = filter(lambda f: f.endswith('.ezr'),contents)
     #give warning if we're missing ezr files, and take only matched vcf's; likely job didn't complete 
     if len(vcf_files) > len(ezr_files):
-        print >> sys.stderr, "WARNING: vcf/ezr numbers do not align, your job probably didn't finish"
+        logging.warning("vcf/ezr numbers do not align, your job probably didn't finish")
         vcf_files = filter(lambda f: ''.join([f[:-4],'.ezr']) in ezr_files, vcf_files) 
    #sort file lists so that when we loop through them in parallel they are in same order
     vcf_files.sort()
@@ -41,9 +49,9 @@ def script(vcf_ezr_path):
             counter += 1
 
     with open(os.path.join(vcf_ezr_path, 'score.txt'), 'w') as file:
-        file.write('Total Patients: ' + str(len(vcf_files)) + '\n')
-        file.write('Total Patients exomizer ranked inserted variant #1: ' + str(counter) + '\n')
-        file.write('Accuracy: ' + str(float(counter)/len(vcf_files)) + '\n')
+        logging.info('Total Patients: ' + str(len(vcf_files)))
+        logging.info('Total Patients exomizer ranked inserted variant #1: ' + str(counter))
+        logging.info('Accuracy: ' + str(float(counter)/len(vcf_files)))
 
 def parse_args(args):
     from argparse import ArgumentParser
