be61025844c857fc945847efc821e735ba22a6ad
Tal Friedman <talf301@gmail.com>
2014-06-23 14:31:04 -0400

    Updated score fetcher to use text files

--- patients/analysis/fetch_scores.py
+++ patients/analysis/fetch_scores.py
@@ -39,11 +39,7 @@ def is_match(linevs, linee):
             return True
     return False
 
-def script(vcf_ezr_paths, A, R, E, D, RD, V, N=None):
-    if E or D:
-        hgmd = HGMD('/dupa-filer/talf/matchingsim/patients/hgmd_correct.jv.vcf')
-    if D:
-        rev_hgmd = hgmd.get_by_omim()
+def script(vcf_ezr_paths, A, R, D, RD, V, N=None):
     if N:
         N=int(N[0])
     for vcf_ezr_path in vcf_ezr_paths:
@@ -58,79 +54,51 @@ def script(vcf_ezr_paths, A, R, E, D, RD, V, N=None):
         Acounter = 0
         Ncounter = 0
         contents = os.listdir(vcf_ezr_path)
-        vcf_files = filter(lambda f: f.endswith('.vcf'),contents)
-        ezr_files = filter(lambda f: f.endswith('.ezr'),contents)
-        #give warning if we're missing ezr files, and take only matched vcf's; likely job didn't complete 
-        if len(vcf_files) > len(ezr_files):
-            logging.warning("vcf/ezr numbers do not align, your job probably didn't finish")
-            vcf_files = filter(lambda f: ''.join([f[:-4],'.ezr']) in ezr_files, vcf_files) 
-       #sort file lists so that when we loop through them in parallel they are in same order
-        vcf_files.sort()
-        ezr_files.sort()
+        txt_files = filter(lambda f: f.endswith('.txt') and any(s == f[:-4] + '.ezr' for s in contents),contents)
         
         if D:
+            hgmd = HGMD('/dupa-filer/talf/matchingsim/patients/hgmd_correct.jv.vcf')
+            rev_hgmd = hgmd.get_by_omim()
             Dcounter = defaultdict(lambda:[0,0]) 
 
-        if E:
-            Ecounter = defaultdict(lambda:[0,0])
-        
         if RD:
             singlecounter = 0
             doublecounter = 0
             singlecorr = 0
             doublecorr = 0
 
-        for vcf, ezr in zip(vcf_files, ezr_files):
-            #if it is a recessive disease we may be pulling 2 lines
-            if R:
-                v = get_last_recessive(os.path.join(vcf_ezr_path,vcf))
-            else:
-                v = get_last_line(os.path.join(vcf_ezr_path,vcf))
-
-            elines = get_actual_lines(os.path.join(vcf_ezr_path,ezr))
-            efirst = elines[0]
+        for txt in txt_files:
+            info = list(open(os.path.join(vcf_ezr_path,txt)))
+            rank = info[0].split(' ')[-1].strip() 
             if RD:
-                singlecounter += len(v) == 1
-                doublecounter += len(v) == 2
-                singlecorr += len(v) == 1 and is_match(v,efirst)
-                doublecorr += len(v) == 2 and is_match(v,efirst)
+                singlecounter += len(info) == 4
+                doublecounter += len(info) == 5
+                singlecorr += len(info) == 4 and rank
+                doublecorr += len(info) == 5 and rank
 
-            if is_match(v,efirst):
-                counter += 1
+            counter += rank == '1'
 
             if D:
-                id = next(x for x in hgmd.entries if x.chrom == v[0].split('\t')[0] and x.loc == v[0].split('\t')[1]).omimid
+                id = info[-2].split(' ')[-1].strip()
                 Dcounter[id][1] += 1
-                if is_match(v,efirst):
-                    Dcounter[id][0] += 1
+                Dcounter[id][0] += rank == '1'
+                if len(Dcounter[id]) == 2:
+                    Dcounter[id].append(info[-1].split(' ')[-1].strip())
 
-            if E:
-                #find the effect of the inserted variant
-                effect = next(x for x in hgmd.entries if x.chrom == v[0].split('\t')[0] and x.loc == v[0].split('\t')[1]).effect
-                Ecounter[effect][1] += 1
-                if is_match(v,efirst):
-                    Ecounter[effect][0] += 1
             if A:
-                if any(is_match(v,x) for x in elines):
-                    Acounter += 1
+                Acounter += rank.isdigit()
             if N:
-                if any(is_match(v,x) for x in elines[:N]):
-                    Ncounter += 1
+                Ncounter += rank.isdigit() and int(rank) <= N
 
-        logging.info('Total Patients: ' + str(len(vcf_files)))
+        logging.info('Total Patients: ' + str(len(txt_files)))
         logging.info('Total Patients exomizer ranked inserted variant #1: ' + str(counter))
-        logging.info('Accuracy of top hits: ' + str(float(counter)/len(vcf_files)) + '\n')
+        logging.info('Accuracy of top hits: ' + str(float(counter)/len(txt_files)) + '\n')
         if N:
             logging.info('Total patients exomizer ranked inserted variant top ' + str(N) + ': ' + str(Ncounter))
-            logging.info('Accuracy of top ' + str(N) + ' hits: ' + str(float(Ncounter)/len(vcf_files))+'\n')
+            logging.info('Accuracy of top ' + str(N) + ' hits: ' + str(float(Ncounter)/len(txt_files))+'\n')
         if A:
             logging.info('Total patients exomizer ranked inserted variant at all: ' + str(Acounter))
-            logging.info('Accuracy of entire file: ' + str(float(Acounter)/len(vcf_files))+'\n')
-        if E:
-            for k,v in Ecounter.iteritems():
-                logging.info('Total patients for effect ' + k + ': ' + str(v[1]))
-                logging.info('Accuracy for effect ' + k + ': ' + str(float(v[0])/v[1]))
-            logging.info('\n')
+            logging.info('Accuracy of entire file: ' + str(float(Acounter)/len(txt_files))+'\n')
         if D: 
             dis = Dcounter.items()
             if V:
@@ -149,6 +117,7 @@ def script(vcf_ezr_paths, A, R, E, D, RD, V, N=None):
                 hits = subprocess.check_output(command,shell=True)
                 logging.info('Orphanet hits for genotypic omim ' + d[0] + ': ' + hits.strip())
                 logging.info('#of variants for genotypic omim ' + d[0] + ': ' + str(len(rev_hgmd[d[0]]))+ '\n')
+                logging.info('Phenotypic omim used in association with this genotypic omim: ' + d[1][2] + '\n')
                 #check what percentage of total is either below 30 or above 60%
                 if d[1][1] > 2:
                     totalcounter += d[1][1]
@@ -176,7 +145,6 @@ def parse_args(args):
     parser.add_argument('-R',help='files to analyze were infected with autosomal recessive diseases (default is AD)', action='store_true')
     parser.add_argument('-A',help='check entire ezr ranking for a hit',action='store_true')
     parser.add_argument('-N',help='check if hit is in the top N entries',nargs=1)
-    parser.add_argument('-E',help='give info about accuracy per variant effect type',action='store_true')
     parser.add_argument('-RD',help='for AR, give info about accuracy for single gene vs. 2 gene', action='store_true')
     parser.add_argument('-D',help='give info about accuracy per disease',action='store_true')
     parser.add_argument('-V',help='when per disease flag is given, sort diseases by number of associated variants',action='store_true')
