b8eb661c538ab51e28dc104492ceec3967476134
Tal Friedman <talf301@gmail.com>
2014-06-26 16:02:00 -0400

    Added script for exomiser 2 processing

---
+++ patients/randompatients/gen_exomiser2_random.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+usage="usage: $0 source_loc num_samples [-R]"
+
+#test for help flag
+if [ $1 == '-h' ]
+then
+    echo "$usage"  
+    exit 1
+fi
+#ensure positional arguments are present
+if [ $# -lt 2 ]; then
+    echo "$usage"
+    exit 1
+fi
+
+set -eu
+set -o pipefail
+#Use date and time as a signature for the generated data
+sig=`date +%F-%H-%M-%S`
+#sge stuff
+memory=10G
+processors=1
+logdir=~/sge_logs/gen_exomise/$sig/
+mkdir -pv $logdir
+if [ $# -eq 3 ] && [ $3 == '-R' ]; then
+    out=/dupa-filer/talf/matchingsim/patients/EX2_"$sig"R
+else
+    out=/dupa-filer/talf/matchingsim/patients/EX2_$sig
+fi
+data=/dupa-filer/talf/matchingsim/patients
+
+delay=80
+
+#location of files given first, number of files to generate is given as second argument
+loc=$1
+num=$2
+#step 1: make copies of some files (randomly choose the number specified)
+mkdir -pv $out
+for s in `ls $loc/*.vcf.gz $loc/*.vcf | sort -R | head -n $num`
+do
+    cp $s $out 
+done
+#step 2: run our patient generator to "infect" all of these patients
+if [ $# -eq 3 ] && [ $3 == '-R' ]; then
+    python $data/randompatients/generate_patients.py $data/phenotype_annotation.tab $data/hgmd_correct.jv.vcf $out $data/orphanet_lookup.xml $data/orphanet_inher.xml $data/orphanet_geno_pheno.xml -I AR
+else
+    python $data/randompatients/generate_patients.py $data/phenotype_annotation.tab $data/hgmd_correct.jv.vcf $out $data/orphanet_lookup.xml $data/orphanet_inher.xml $data/orphanet_geno_pheno.xml -I AD
+fi
+#step 3: create a script and dispatch exomizer job (and create rerun script)
+cat > "$out/rerun.sh" <<EOF
+for file in $out/*.vcf
+do
+    f=\`echo \$file | rev | cut -d '/' -f1 | rev | cut -d '.' -f1\`
+    #resubmit script only if the required file doesn't already exist
+    if [ ! -f "$out"/\$f.ezr ]
+    then
+        qsub -S /bin/sh "$out/scripts/dispatch_\$f.sh"
+        sleep $delay
+    fi
+done
+EOF
+chmod +x "$out/rerun.sh"
+
+mkdir -pv $out/scripts
+for file in $out/*.vcf.gz $out/*.vcf; do
+    #create a bash script
+    #get only ending to name script
+    f=`echo $file | rev | cut -d '/' -f1 | rev | cut -d '.' -f1`
+    script="$out/scripts/dispatch_$f.sh"   
+    if [ $# -eq 3 ] && [ $3 == '-R' ]; then
+        cat > "$script" <<EOF
+#!/usr/bin/env bash
+#$ -V
+#$ -N "$f"
+#$ -pe parallel "$processors"
+#$ -l h_vmem="$memory"
+#$ -e $logdir
+#$ -o $logdir
+
+set -eu
+set -o pipefail
+temp=\$TMPDIR/$f.ezr
+
+#only unzip if the unziped file doesn't already exist (i.e. only unzip on first run)
+if [ ! -f "$out"/$f.vcf ]
+then
+    gunzip $out/$f.vcf.gz
+fi
+java -Xms5g -Xmx5g -jar /filer/tools/exomiser/2.0.0/exomiser-2.0.0.jar -D /filer/tools/exomiser/2.0.0/data/ucsc_hg19.ser -I AR -F 1 -W /filer/tools/exomiser/2.0.0/data/rw_string_9_05.gz -X /filer/tools/exomiser/2.0.0/data/rw_string_9_05_id2index.gz --hpo_ids `cat $out/"$f"_hpo.txt` -v $out/$f.vcf --vcf_output -o \$temp -P
+
+mv -v \$temp $out/$f.ezr.temp
+mv -v $out/$f.ezr.temp $out/$f.ezr
+EOF
+    else
+        cat > "$script" <<EOF
+#!/usr/bin/env bash
+#$ -V
+#$ -N "$f"
+#$ -pe parallel "$processors"
+#$ -l h_vmem="$memory"
+#$ -e $logdir
+#$ -o $logdir
+
+set -eu
+set -o pipefail
+temp=\$TMPDIR/$f.ezr
+
+#only unzip if the unziped file doesn't already exist (i.e. only unzip on first run)
+if [ ! -f "$out"/$f.vcf ]
+then
+    gunzip $out/$f.vcf.gz
+fi
+java -Xms5g -Xmx5g -jar /filer/tools/exomiser/2.0.0/exomiser-2.0.0.jar -D /filer/tools/exomiser/2.0.0/data/ucsc_hg19.ser -I AD -F 1 -W /filer/tools/exomiser/2.0.0/data/rw_string_9_05.gz -X /filer/tools/exomiser/2.0.0/data/rw_string_9_05_id2index.gz --hpo_ids `cat $out/"$f"_hpo.txt` -v $out/$f.vcf --vcf_output -o \$temp -P
+
+mv -v \$temp $out/$f.ezr.temp
+mv -v $out/$f.ezr.temp $out/$f.ezr
+EOF
+    fi
+    #Submit
+    qsub -S /bin/sh "$script"
+    #wait so we don't overload cluster
+    sleep $delay
+done
+
